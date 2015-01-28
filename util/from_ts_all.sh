f796686b80696d43a50865bf799b20c07f6ce15f
Lennart Kats <lclkats@gmail.com>
2015-01-28 17:08:15 +0100

    Create more sig files

--- util/from_ts_all.sh
+++ util/from_ts_all.sh
@@ -13,6 +13,7 @@ cd $MY_DIR
 mkdir -p sigs
 cd sigs
 echo $REVISION > revision.txt
+cp ../sigs_ts/LICENSE .
 
 if ! which parallel &> /dev/null; then
     echo "Please install GNU parallel" >&2
@@ -24,6 +25,7 @@ ls ../sigs_ts | parallel '
 
     find_main_file() {
         local DIR=$1
+        local NAME=$2
         try() {
             if [ -e $1 ]; then
                 echo "$1"
@@ -35,17 +37,21 @@ ls ../sigs_ts | parallel '
         try $DIR/$DIR.d.ts ||
         try $DIR/angular.d.ts ||
         try `ls $DIR/*.d.ts` ||
-        {
-            echo "Warning: could not find main signature file for $DIR" >&2
-        }
+        echo "$NAME.json - could not find main signature file :("
     }
     
     if [ ! -d $DIR ]; then
         exit
     fi
     NAME=$(basename $DIR)
-    MAIN=$(find_main_file $DIR)
+    MAIN=$(find_main_file $DIR $NAME)
     if [ "$MAIN" ]; then
-        '$MY_DIR'/from_ts.sh "$MAIN" $NAME.json
+        bash '$MY_DIR'/from_ts.sh $MAIN $NAME.json
     fi
+    for OTHER in $DIR/*.d.ts; do
+        if [ "$OTHER" == "$MAIN" ]; then
+            continue
+        fi
+        bash -e '$MY_DIR'/from_ts.sh $OTHER _$(basename $DIR)_$(basename $OTHER .d.ts).json
+    done
 '
