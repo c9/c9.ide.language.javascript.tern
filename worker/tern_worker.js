68875bc113d0eb9b2599a3ab74c1f381d6f63dac
Lennart Kats <lclkats@gmail.com>
2015-08-09 12:11:15 +0200

    Fix parsing new tern type signatures

--- worker/tern_worker.js
+++ worker/tern_worker.js
@@ -568,9 +568,9 @@ function getSignature(property) {
     var inReturn = false;
     for (var i = "fn(".length; i < sig.length; i++) {
         switch (sig[i]) {
-            case "(":
+            case "(": case "{":
                 depth++; break;
-            case ")":
+            case ")": case "}":
                 depth--; break;
             case ":":
                 inType = true; break;
