9dbb4f19435f2bac8e0e9c3b19504c7653eac858
Lennart Kats <lclkats@gmail.com>
2015-12-01 11:56:55 +0000

    Cleanup

--- tern.js
+++ tern.js
@@ -28,42 +28,7 @@ define(function(require, exports, module) {
                 // TODO: register "extra" defs?
             }
             
-            var ternOptions = options.tern || {
-                plugins: {
-                    // UNDONE: doesn't normally exist; should be added by config
-                    // smartface: "plugins/smartface/loadInclude.js",
-                    angular: "tern/plugin/angular",
-                    commonjs: "tern/plugin/commonjs",
-                    doc_comment: "tern/plugin/doc_comment",
-                    es_modules: "tern/plugin/es_modules",
-                    modules: "tern/plugin/modules",
-                    node: "tern/plugin/node",
-                    node_resolve: "tern/plugin/node_resolve",
-                    requirejs: "tern/plugin/requirejs",
-                    architect_resolver: "./architect_resolver_worker",
-                },
-                defs: [{
-                    name: "ecma5",
-                    enabled: true,
-                    path: "lib/tern/defs/ecma5.json"
-                }, {
-                    name: "jQuery",
-                    enabled: true,
-                    path: "lib/tern/defs/jquery.json"
-                }, {
-                    name: "browser",
-                    enabled: true,
-                    path: "lib/tern/defs/browser.json"
-                }, {
-                    name: "underscore",
-                    enabled: false,
-                    path: "lib/tern/defs/underscore.json"
-                }, {
-                    name: "chai",
-                    enabled: false,
-                    path: "tern/defs/chai.json"
-                }]
-            };
+            var ternOptions = options.tern;
 
             ternPlugins(function callback(e) {
                 var pluginName;
